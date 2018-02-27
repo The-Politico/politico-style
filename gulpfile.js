2ac82ad67dfe6fb1fd5cc949f203e9581d26bcc1
Jon McClure <jmcclure@politico.com>
2018-02-26 22:23:50 -0500

    better gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const webserver = require('gulp-webserver');
+const connect = require('gulp-connect');
+const open = require("open");
 const sass = require('gulp-sass');
 const nunjucksRender = require('gulp-nunjucks-render');
 const tildeImporter = require('node-sass-tilde-importer');
@@ -10,12 +11,12 @@ portfinder.basePort = 3000;
 gulp.task('server', () =>
   portfinder.getPortPromise()
     .then((foundPort) => {
-      gulp.src('docs')
-        .pipe(webserver({
-          livereload: true,
-          open: true,
-          port: foundPort
-        }))
+      connect.server({
+        root: ['docs'],
+        port: foundPort,
+        livereload: true,
+      });
+      open(`http://localhost:${foundPort}`);
     })
 );
 
@@ -23,16 +24,20 @@ gulp.task('scss', () =>
   gulp.src('./scss/**/*.scss')
     .pipe(sass({ importer: tildeImporter }))
     .pipe(gulp.dest('./docs/css'))
+    .pipe(connect.reload())
 );
-
-gulp.task('scss:watch', () => gulp.watch('./scss/**/*.scss', ['scss']));
 
 gulp.task('templates', () =>
     gulp.src('templates/**/index.html')
         .pipe(nunjucksRender({ path: ['templates/'] }))
         .pipe(gulp.dest('docs'))
+        .pipe(connect.reload())
 );
 
-gulp.task('templates:watch', () => gulp.watch('./templates/**/*.html', ['templates']));
+gulp.task('scss:watch', () => gulp.watch('./scss/**/*.scss', gulp.series('scss')));
 
-gulp.task('default', ['templates', 'scss', 'scss:watch', 'templates:watch', 'server']);
+gulp.task('templates:watch', () => gulp.watch('./templates/**/*.html', gulp.series('templates')));
+
+gulp.task('dev', gulp.parallel('scss:watch', 'templates:watch', 'server'));
+
+gulp.task('default', gulp.series('templates', 'scss', 'dev'));
