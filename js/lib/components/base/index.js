ee3397a51fcf1aac5538fc8834d9f0a9ca473b31
Andrew Briz <briz.andrew@gmail.com>
2019-01-10 17:38:04 -0500

    Exporting container.

--- js/lib/components/base/index.js
+++ js/lib/components/base/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from './Footer';
 export { default as Head } from './Head';
 export { default as Header } from './Header';
 export { default as Navigation } from './Navigation';
+export { default as Container } from './Container';
