fc5c637704670db9422a3cf9b1bd2d3336c39bf2
Andrew Briz <briz.andrew@gmail.com>
2019-01-10 16:35:07 -0500

    Pass through imports for new base components.

---
+++ js/lib/components/base/index.js
@@ -0,0 +1,5 @@
+export { default as Ad } from './Ad';
+export { default as Footer } from './Footer';
+export { default as Head } from './Head';
+export { default as Header } from './Header';
+export { default as Navigation } from './Navigation';
