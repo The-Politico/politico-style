454678d152ab9265dd01e7b036369c128e852c85
Andrew Briz <briz.andrew@gmail.com>
2019-01-10 17:36:04 -0500

    Added container component.

---
+++ js/lib/components/base/Container.jsx
@@ -0,0 +1,43 @@
+import {default as React, Fragment} from 'react';
+
+import { Head, Navigation, Footer, Header, Ad } from './';
+
+const Container = props => {
+  const { meta, social, content } = props;
+
+  return (
+    <Fragment>
+      <Head title={meta.pageName} social={social} />
+      <Navigation />
+      <Ad.Banner />
+      <Header
+        headline={content.headline}
+        deck={content.deck}
+        section={content.section}
+        authors={meta.authors}
+        published={meta.published}
+        updated={meta.updated}
+      />
+
+      <p>This page only works with hydration.</p>
+      <Footer />
+    </Fragment>
+  );
+};
+
+Container.defaultProps = {
+  social: {},
+  meta: {
+    pageName: '',
+    authors: [],
+    published: null,
+    updated: null,
+  },
+  content: {
+    headline: '',
+    deck: '',
+    section: '',
+  },
+};
+
+export default Container;
